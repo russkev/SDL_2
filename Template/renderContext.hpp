86699b9591b548db6f4d1524dd8fb9fbcd481206
Kevin Russell - Surface <kev_kix@hotmail.com>
2016-10-13 09:18:25 +0100

    syntax error 'constant'

--- Template/renderContext.hpp
+++ Template/renderContext.hpp
@@ -6,6 +6,7 @@ namespace graphics {
 	template<typename _View>
 	struct renderContext{
 		typedef _View view_type;
+		typedef std::vector<int> buffer_type;
 
 		renderContext(view_type& s_view) :
 			s_view(m_view)
@@ -13,7 +14,7 @@ namespace graphics {
 
 	private:
 		view_type& m_view;
-		std::vector<int> scanBuffer;// (5);//m_view.size().y*2);
+		buffer_type scanBuffer ((m_view.size().y*2),0);
 
 
 	public:
