7473cdb462e546fa8215fe61c6e832aaf9fd56ee
Kevin Russell - Surface <kev_kix@hotmail.com>
2017-01-05 07:29:41 +0000

    Updated vertex constructors

--- Template/vertex.hpp
+++ Template/vertex.hpp
@@ -6,14 +6,19 @@ namespace graphics {
 	struct vertex {
 		typedef tvec4<std::uint8_t> bgra_color_type;
 		typedef vec4 point_type;
+		typedef vec2 coord_type;
+
 	public:
 		point_type m_pos;
 		bgra_color_type m_col;
+		coord_type m_coord;
+
 
 		// // Constructors
-		vertex(const point_type& s_pos, const bgra_color_type& s_col) :m_pos(s_pos), m_col(s_col) {}
-		vertex(const point_type& s_pos) :vertex(s_pos, bgra_color_type(0,0,0,255)) {}
-		vertex(float x, float y, float z, float w) : vertex(point_type(x,y,z,w)) {}
+		vertex(const point_type& s_pos, const coord_type& s_coord, const bgra_color_type& s_color) :m_pos(s_pos), m_col(s_color), m_coord(s_coord) {}
+		vertex(const point_type& s_pos, const coord_type& s_coord) : vertex(s_pos, s_coord, bgra_color_type(0, 0, 0, 0)) {}
+		vertex(const point_type& s_pos, const bgra_color_type& s_color) : vertex(s_pos, coord_type(0, 0), s_color) {}
+		vertex(const point_type& s_pos) : vertex(s_pos, coord_type(0, 0)) {}
 
 		
 
