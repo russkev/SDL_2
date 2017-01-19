0c4bb76b5a5a50f90c9637f2370f9d9f31cb9d4d
Kev <Kev@KEV-PC>
2017-01-19 08:27:57 +0000

    Added existing obj loader to learn from

--- Template/obj.hpp
+++ Template/obj.hpp
@@ -8,22 +8,28 @@
 namespace graphics {
 	struct obj {
 		typedef vec3 obj_vertex_type;
+		typedef vec2 obj_uv_type;
 	private:
 		std::vector<std::string> m_file_contents;
 		std::vector<obj_vertex_type> m_vertices;
-		std::string m_buffer;
+		std::vector<obj_vertex_type> m_normals;
+		std::vector<obj_uv_type> m_uvs;
+		std::vector<std::vector<ivec3>> m_faces;
+		
 	public:
 		obj(const char* file_name) {
+			std::string buffer;
+
 			std::ifstream file(file_name);
 			if (!file)
 			{
 				perror("File does not compute");
 			}
 
-			while (std::getline(file, m_buffer)) {
-				if (m_buffer[0] != '#' || m_buffer[0] != ' ')
+			while (std::getline(file, buffer)) {
+				if (buffer[0] != '#' || buffer[0] != ' ')
 				{
-					m_file_contents.push_back(m_buffer);
+					m_file_contents.push_back(buffer);
 				}
 			}
 
@@ -33,11 +39,37 @@ namespace graphics {
 
 			for (unsigned int n = 0; n < m_file_contents.size(); n++)
 			{
+				// // Add vertices // //
 				if (m_file_contents[n].c_str()[0] == 'v' && m_file_contents[n].c_str()[1] == ' ')
 				{
 					float x, y, z;
 					sscanf(m_file_contents[n].c_str(), "v %f %f %f", &x, &y, &z);
 					m_vertices.push_back(obj_vertex_type(x, y, z));
+				}
+				// // Add UVs // //
+				if (m_file_contents[n].c_str()[0] == 'v' && m_file_contents[n].c_str()[1] == 't')
+				{
+					float u, v;
+					sscanf(m_file_contents[n].c_str(), "vt %f %f", &u, &v);
+					m_uvs.push_back(obj_uv_type(u, v));
+				}
+				// // Add normals // //
+				if (m_file_contents[n].c_str()[0] == 'v' && m_file_contents[n].c_str()[1] == 'n')
+				{
+					float x, y, z;
+					sscanf(m_file_contents[n].c_str(), "vn %f %f %f", &x, &y, &z);
+					m_normals.push_back(obj_vertex_type(x, y, z));
+				}
+				// // Add faces // //
+				if (m_file_contents[n].c_str()[0] == 'f' && m_file_contents[n].c_str()[1] == ' ')
+				{
+					int p = 3;
+					while (m_file_contents[n].c_str()[p] == '/') {
+
+					}
+					//float x, y, z;
+					//sscanf(m_file_contents[n].c_str(), "f %f %f", &u, &v);
+					//m_faces.push_back(obj_uv_type(u, v));
 				}
 			}
 
